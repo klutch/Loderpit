56f0841bc51f3ac12e7aad9fbc51ca386bd3e0fc
Graeme Collins <graeme.collins.0@gmail.com>
2013-11-24 04:23:54 -0600

    Added callbacks.

--- Loderpit/Loderpit/SpellEffects/SpellEffect.cs
+++ Loderpit/Loderpit/SpellEffects/SpellEffect.cs
@@ -15,6 +15,8 @@ namespace Loderpit.SpellEffects
         private bool _affectsNeutral;
         private bool _affectsHostile;
         private int _timeToLive;   // -1 == infinite, 0 == dead, > 0 == alive
+        private Action<int, int> _onHitByOther;
+        private Action<int, int> _onHitOther;
 
         public SpellEffectType type { get { return _type; } }
         public bool affectsSelf { get { return _affectsSelf; } }
@@ -22,13 +24,32 @@ namespace Loderpit.SpellEffects
         public bool affectsNeutral { get { return _affectsNeutral; } }
         public bool affectsHostile { get { return _affectsHostile; } }
 
-        public SpellEffect(SpellEffectType type, bool affectsSelf, bool affectsFriendly, bool affectsNeutral, bool affectsHostile, int timeToLive)
+        public SpellEffect(SpellEffectType type, bool affectsSelf, bool affectsFriendly, bool affectsNeutral, bool affectsHostile, int timeToLive, Action<int, int> onHitByOther, Action<int, int> onHitOther)
         {
             _type = type;
             _affectsSelf = affectsSelf;
             _affectsFriendly = affectsFriendly;
+            _affectsNeutral = affectsNeutral;
             _affectsHostile = affectsHostile;
             _timeToLive = timeToLive;
+            _onHitByOther = onHitByOther;
+            _onHitOther = onHitOther;
+        }
+
+        public void onHitByOther(int attackerId, int defenderId)
+        {
+            if (_onHitByOther != null)
+            {
+                _onHitByOther(attackerId, defenderId);
+            }
+        }
+
+        public void onHitOther(int attackerId, int defenderId)
+        {
+            if (_onHitOther != null)
+            {
+                _onHitOther(attackerId, defenderId);
+            }
         }
     }
 }
